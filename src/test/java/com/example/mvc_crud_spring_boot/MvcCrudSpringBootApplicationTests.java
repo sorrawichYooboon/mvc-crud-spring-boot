1e9a7373e0ceacfbd635583690807e34c2d98de3
Sorrawich Yooboon (Ko) <sorrawich.yoob@kkpfg.com>
2025-01-19 16:05:34 +0700

    feat:init project

---
+++ src/test/java/com/example/mvc_crud_spring_boot/MvcCrudSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvc_crud_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcCrudSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
